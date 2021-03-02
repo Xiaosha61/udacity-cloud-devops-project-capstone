bdde0a8cd4fc8d7d785cde28f8e3a3e88492d36a
Xiaosha Xing <xiaosha.xing@logmein.com>
2021-03-02 23:05:45 +0100

    chore: introduce eslint

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const PORT = 3000;
 
 app.use(async ctx => {
     console.log(`Received ${ctx.method} request for: ${ctx.path}.`);
-    switch(ctx.path) {
-        case '/env':
-            ctx.body = process.env;
-            break;
-        default:
-            ctx.body = 'Welcome to my dummy node service.';
-            break;
+    switch (ctx.path) {
+    case '/env':
+        ctx.body = process.env;
+        break;
+    default:
+        ctx.body = 'Welcome to my dummy node service.';
+        break;
     }
 });
 
 app.listen(PORT, () => {
+    debugger;
     console.log(`Server starts at port ${PORT}...`);
 });
